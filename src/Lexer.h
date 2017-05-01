1a323eb3dd3418fbf5ab09b6f6637a35547185d0
Michael <michael.vella.14@um.edu.mt>
2017-05-01 15:04:40 +0200

    added struct Token with TokenType, name and value

--- src/Lexer.h
+++ src/Lexer.h
@@ -3,4 +3,13 @@
 
 enum TokenType { TOK_EOF, TOK_NUMBER, TOK_ARITHMETIC_OP };
 
+struct Token {
+    TokenType tokenType;
+    std::string name;
+    float value;
+    
+    
+};
+
+
 #endif /* LEXER_H */
